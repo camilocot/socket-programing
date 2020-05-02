f498617a0754b5269feeee436b1823f0915e3812
Camilo <ccota@theworkshop.com>
2020-05-02 17:26:00 +0200

    UDP: multicast ping/pong server

--- udp/multicast/receiver.py
+++ udp/multicast/receiver.py
@@ -1,22 +1,34 @@
-# chat server using multicast
+# ping/pong server using multicast
 # receiver.py
 # usage : $ python receiver.py  # wait for messages to come in
 
 import socket
 import struct
+import sys
 
 multicast_addr = '224.0.0.1'
-bind_addr = '0.0.0.0'
+
 port = 3000
+server_address = ('', port)
 
+# Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
 
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
+# Bind to the server address
+sock.bind(server_address)
+
+# Tell the operating system to add the socket to the multicast group
+# on all interfaces.
+group = socket.inet_aton(multicast_addr)
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock.bind((bind_addr, port))
-
+# Receive/respond loop
 while True:
-    message, address = sock.recvfrom(255)
-    print(message)
+    data, address = sock.recvfrom(1024)
+
+    print('received: %s with length %s bytes, data' % (data, len(data)))
+    print('sending acknowledgement to {}'.format(address))
+
+    sock.sendto(b'ack', address)
--- udp/multicast/send.py
+++ udp/multicast/send.py
@@ -1,4 +1,4 @@
-# chat server using multicast
+# ping/pong client using multicast
 # send.py
 # usage : $ python send.py message
 
@@ -6,13 +6,37 @@ import socket
 import struct
 import sys
 
-message = sys.argv[1] if len(sys.argv) > 1 else 'message via multicast'
+message = sys.argv[1] if len(sys.argv) > 1 else 'ping via multicast'
 
 multicast_addr = '224.0.0.1'
 port = 3000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set a timeout so the socket does not block indefinitely when trying
+# to receive data.
+sock.settimeout(0.2)
+# Multicast datagrams are sent with a default TTL value of 1, to prevent them to be forwarded beyond the local network.
+# and flooding network segments
 ttl = struct.pack('b', 1)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-sock.sendto(bytes(message, encoding='utf-8'), (multicast_addr, port))
-sock.close()
+
+
+try:
+
+    # Send data to the multicast group
+    print('sending "%s"' % message)
+    sock.sendto(bytes(message, encoding='utf-8'), (multicast_addr, port))
+
+    # Look for responses from all recipients
+    while True:
+        try:
+            data, server = sock.recvfrom(16)
+        except socket.timeout:
+            print('timed out, no more responses', file=sys.stderr)
+            break
+        else:
+            print('received "%s" from %s' % (data, server))
+
+finally:
+    sock.close()
